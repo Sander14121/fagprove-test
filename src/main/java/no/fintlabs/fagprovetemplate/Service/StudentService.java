a9ea3f4889865f54354a935ba4c6285affbaa3f7
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2025-04-15 13:40:13 +0200

    add cryptation on student identity number

--- src/main/java/no/fintlabs/fagprovetemplate/Service/StudentService.java
+++ src/main/java/no/fintlabs/fagprovetemplate/Service/StudentService.java
@@ -1,5 +1,6 @@
 package no.fintlabs.fagprovetemplate.Service;
 
+import lombok.extern.slf4j.Slf4j;
 import no.fintlabs.fagprovetemplate.Repository.StudentRepository;
 import no.fintlabs.fagprovetemplate.model.dto.Student;
 import no.fintlabs.fagprovetemplate.model.entity.StudentEntity;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 public class StudentService {
 
@@ -20,9 +22,13 @@ public class StudentService {
     }
 
     public String addStudent(Student student) {
-        StudentEntity studentEntity = mapper.mapStudent(student);
-        studentRepository.save(studentEntity);
-        return "Saved student with id " + studentEntity.getId();
+        try {
+            StudentEntity studentEntity = mapper.mapStudent(student);
+            studentRepository.save(studentEntity);
+            return "Saved student with id " + studentEntity.getId();
+        } catch (Exception e) {
+            return "Error saving student with id " + student.getId() + " with cause " + e.getMessage();
+        }
     }
 
     public List<Student> getAll() {
@@ -30,4 +36,10 @@ public class StudentService {
                 .stream()
                 .map(entity -> mapper.mapToStudentDto(entity)).collect(Collectors.toList());
     }
+
+    public Student getById(String id) {
+        StudentEntity byId = studentRepository.getById(Long.parseLong(id));
+        return mapper.mapToStudentDto(byId);
+    }
+
 }
