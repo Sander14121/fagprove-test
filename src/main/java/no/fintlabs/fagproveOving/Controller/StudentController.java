722a79718a4e8687770d6587c5b449f93b9a1680
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2025-04-22 10:49:08 +0200

    returns conflict if student alredy exists

--- src/main/java/no/fintlabs/fagproveOving/Controller/StudentController.java
+++ src/main/java/no/fintlabs/fagproveOving/Controller/StudentController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import no.fintlabs.fagproveOving.Service.StudentService;
 import no.fintlabs.fagproveOving.model.dto.Student;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,6 +24,7 @@ public class StudentController {
 
     @PostMapping
     public ResponseEntity<Student> create(@RequestBody Student student, HttpServletRequest request) {
+        if (studentService.exists(student.getId())) return new ResponseEntity<>(HttpStatus.CONFLICT);
         Student savedStudent = (Student) studentService.addStudent(student);
         URI location = URI.create(String.format("%s/%s", request.getRequestURL(), savedStudent.getId()));
         return ResponseEntity.created(location).body(savedStudent);
